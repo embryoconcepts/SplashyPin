680426ec7f2f4b5b84b50a46102d691962490848
Jen Hamilton <jen@jshamilton.net>
2020-10-05 14:41:31 -0400

    Initial Commit

---
+++ SplashyPin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplashyPin
+//
+//  Created by Jennifer Hamilton on 10/5/20.
+//  Copyright © 2020 Jennifer Hamilton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SplashyPinTests/SplashyPinTests.swift
@@ -0,0 +1,34 @@
+//
+//  SplashyPinTests.swift
+//  SplashyPinTests
+//
+//  Created by Jennifer Hamilton on 10/5/20.
+//  Copyright © 2020 Jennifer Hamilton. All rights reserved.
+//
+
+import XCTest
+@testable import SplashyPin
+
+class SplashyPinTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
